0808b8622f16b1c6512320e138b44ab9e6e585fb
Korobeynik <korobeynik77@gmail.com>
2018-12-05 21:59:48 +0200

    Project init

---
+++ app/js/common.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    // var owl = $('.owl-carousel');
+    // owl.owlCarousel({
+    //     animateOut: 'slideOutDown',
+    //     animateIn: 'fadeIn',
+    //     items:1,
+    //     loop:true,
+    //     margin: 0,
+    //     smartSpeed: 500,
+    //     //autoplay: true,
+    //     autoplayTimeout: 20000,
+    //     autoplayHoverPause: false
+    // }).on('translate.owl.carousel', function() {
+    //    $(this).find('.composition-text').removeClass('animated fadeInUp');
+    // }).on('translated.owl.carousel', function() {
+    //    $(this).find('.composition-text').addClass('animated fadeInUp');
+    // });
+
+    // $('.owl-carousel .composition-text').on('mouseleave',function(e){
+    //     $(this).closest('.owl-carousel').trigger('play.owl.autoplay');
+    // })
+
+    // $('.owl-carousel .composition-text').on('mouseenter',function(e){
+    //     $(this).closest('.owl-carousel').trigger('stop.owl.autoplay');
+    // })
+
+});
