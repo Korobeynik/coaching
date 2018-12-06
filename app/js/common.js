6fd5b880428016117bb0534c538af05bd141c5ba
Korobeynik <korobeynik77@gmail.com>
2018-12-06 06:06:30 +0200

    Header section, slider, form

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,16 @@
 $(function() {
 
-    // var owl = $('.owl-carousel');
-    // owl.owlCarousel({
-    //     animateOut: 'slideOutDown',
-    //     animateIn: 'fadeIn',
-    //     items:1,
-    //     loop:true,
-    //     margin: 0,
-    //     smartSpeed: 500,
-    //     //autoplay: true,
-    //     autoplayTimeout: 20000,
-    //     autoplayHoverPause: false
-    // }).on('translate.owl.carousel', function() {
-    //    $(this).find('.composition-text').removeClass('animated fadeInUp');
-    // }).on('translated.owl.carousel', function() {
-    //    $(this).find('.composition-text').addClass('animated fadeInUp');
-    // });
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin: 0,
+        smartSpeed: 500,
+        //autoplay: true,
+        nav: true,
+        autoplayTimeout: 20000,
+        autoplayHoverPause: false
+    })
 
     // $('.owl-carousel .composition-text').on('mouseleave',function(e){
     //     $(this).closest('.owl-carousel').trigger('play.owl.autoplay');
